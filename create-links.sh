a94bc4e21bb6ea3acbb924b4aea60117e7eb1aac
iarigby <marvinzem@gmail.com>
2019-05-08 00:05:27 +0200

    improved script that doesn't work

--- create-links.sh
+++ create-links.sh
@@ -1,17 +1,24 @@
+#does not work
+
 visit_dir() {
     echo "entered on $1"
+    sourcedir=$1
+    destination=$2
     for file in $(ls -A $1);
     do
-	if [ -d "$1/$file" ];
+	echo $file
+	if [ -d "$sourcedir/$file" ];
 	then
-	    echo "visiting $1/$file"
-	    visit_dir "$1/$file"
+	    echo "visiting $sourcedir/$file"
+	    visit_dir "$sourcedir/$file" "$destination/$file"
 	else
-	    echo " dir $1 file $file"
+	    echo "     dir $sourcedir file $file destination dir $destination"
+	   # mkdir -p $destination
+	    # ln -s "$sourcedir/$file" "$destination/$file"  
 	    # use basename to copy 
-	    bat "$1/$file"
+	    # bat "$sourcedir/$file"
 	fi
     done
 }
 
-visit_dir "/home/$USER/dotfiles/home"
+visit_dir "/home/$USER/dotfiles/home" "/home/$USER"
