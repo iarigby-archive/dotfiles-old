1e37e48f0113d4f6ae6b0a4fa6c8ab1d3ffd2680
iarigby <marvinzem@gmail.com>
2019-07-18 22:57:12 +0400

    doom

--- home/.doom.d/config.el
+++ home/.doom.d/config.el
@@ -15,7 +15,21 @@
                "\\documentclass{article}"
                ("\\section{%s}" . "\\section*{%s}")))
 
+;; https://lists.gnu.org/archive/html/emacs-orgmode/2009-10/msg00276.html
 (setq org-agenda-files
 (split-string
 (shell-command-to-string " fd -d 3 -t f -t l .org$ ~/org")
 "\n"))
+
+(setq org-capture-templates `(
+	("s" "Protocol" entry (file+headline ,(concat org-directory "notes.org") "Inbox")
+        "* %^{Title}\nSource: %u, %c\n #+BEGIN_QUOTE\n%i\n#+END_QUOTE\n\n\n%?")
+	("L" "Protocol Link" entry (file+headline ,(concat org-directory "notes.org") "Inbox")
+        "* %? [[%:link][%:description]] \nCaptured On: %U")
+    ("p" "Protocol snippet" entry (file, (concat org-directory "snippets.org") )
+     "* %? [[%:link][%:description]] \n  #+BEGIN_SRC %^{Language}\n%i\n#+END_SRC\n\n\n")
+    ))
+
+(add-hook! 'org-mode
+  (local-set-key (kbd "C-RET") 'org-insert-todo-heading))
+
