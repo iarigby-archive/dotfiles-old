f12d4df354451f16df59b210d105c537d972f62a
iarigby <marvinzem@gmail.com>
2019-09-19 21:33:49 +0400

    learned to configure doom

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -32,4 +32,35 @@
 
 (add-hook! 'org-mode
   (local-set-key (kbd "C-RET") 'org-insert-todo-heading))
+(require 'openwith)
 
+(openwith-mode t)
+
+(setq openwith-associations '(("\\.pdf\\'" "foxitreader" (file))))
+
+;; begin doom themes
+(require 'doom-themes)
+
+;; Global settings (defaults)
+(setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+      doom-themes-enable-italic t) ; if nil, italics is universally disabled
+
+;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each theme
+;; may have their own settings.
+
+(load-theme 'doom-outrun-electric t)
+;; Enable flashing mode-line on errors
+(doom-themes-visual-bell-config)
+
+;; Enable custom neotree theme (all-the-icons must be installed!)
+(doom-themes-neotree-config)
+;; or for treemacs users
+(setq doom-themes-treemacs-theme "doom-colors") ; use the colorful treemacs theme
+(doom-themes-treemacs-config)
+
+;; Corrects (and improves) org-mode's native fontification.
+(doom-themes-org-config)
+
+;; end doom themes
+;;
+(require 'material-theme)
